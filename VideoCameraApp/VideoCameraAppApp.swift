2057f8af38eaaf9a8078ea2412cfbb208df4e09e
ReggieGrant <grantreggie09@gmail.com>
2026-05-25 13:45:25 -0700

    Initial Commit

---
+++ VideoCameraApp/VideoCameraAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoCameraAppApp.swift
+//  VideoCameraApp
+//
+//  Created by Reginald Grant on 5/25/26.
+//
+
+import SwiftUI
+
+@main
+struct VideoCameraAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
